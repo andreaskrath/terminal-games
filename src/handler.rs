3df072c069b747c13bb078663e80c6efcccbe2aa
Andreas Krath <andreas.krath+github@gmail.com>
2023-07-23 00:33:21 +0200

    Add tui to handler parameters, as handler is responsible for launching sub-programs

--- src/handler.rs
+++ src/handler.rs
@@ -1,8 +1,17 @@
-use crate::app::{App, AppResult};
+use crate::{
+    app::{App, AppResult},
+    chess::Chess,
+    tui::Tui,
+};
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use tui::prelude::Backend;
 
 /// Handles the key events and updates the state of [`App`].
-pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
+pub fn handle_key_events<B: Backend>(
+    key_event: KeyEvent,
+    app: &mut App,
+    tui: &mut Tui<B>,
+) -> AppResult<()> {
     match key_event.code {
         KeyCode::Esc | KeyCode::Char('q') => {
             app.quit();
@@ -18,6 +27,14 @@ pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
         KeyCode::Down => {
             app.move_down();
         }
+        KeyCode::Enter => match app.state.selected().expect("should always have a state") {
+            0 => {
+                let mut chess_game = Chess::new(tui);
+                chess_game.launch_chess()?;
+            }
+            1 => {}
+            _ => {}
+        },
         _ => {}
     }
     Ok(())
