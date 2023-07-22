4252b0b2bd8782570637e04838342e67168bea25
Andreas Krath <andreas.krath+github@gmail.com>
2023-07-23 00:34:20 +0200

    Define central functionality of chess module, and sub-modules

---
+++ src/chess/mod.rs
@@ -0,0 +1,43 @@
+use crate::{app::AppResult, event::Event, tui::Tui};
+use tui::prelude::Backend;
+
+use self::{game::Game, handler::handle_key_events};
+
+mod game;
+mod handler;
+mod ui;
+
+pub struct Chess<'a, B: Backend> {
+    running: bool,
+    tui: Option<&'a mut Tui<B>>,
+    game: Game,
+}
+
+impl<'a, B: Backend> Chess<'a, B> {
+    pub fn new(tui: &'a mut Tui<B>) -> Self {
+        Self {
+            running: true,
+            tui: Some(tui),
+            game: Game::new(),
+        }
+    }
+
+    pub fn quit(&mut self) {
+        self.running = false;
+    }
+
+    pub fn launch_chess(&mut self) -> AppResult<()> {
+        while self.running {
+            let tui = self.tui.take().expect("should always contain a tui");
+            tui.terminal.draw(|frame| ui::render(self, frame))?;
+            match tui.events.next()? {
+                Event::Tick => {}
+                Event::Key(key_event) => handle_key_events(key_event, self)?,
+                Event::Mouse(_) => {}
+                Event::Resize(_, _) => {}
+            }
+            self.tui = Some(tui);
+        }
+        Ok(())
+    }
+}
