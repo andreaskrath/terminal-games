c8ac4e46655e3d32d4ca2b1e1128b42ccfee1402
Andreas Krath <andreas.krath+github@gmail.com>
2023-07-24 03:20:48 +0200

    Add board rendering and player nameplates; currently dummy values

--- src/chess/ui.rs
+++ src/chess/ui.rs
@@ -1,35 +1,58 @@
 use super::Chess;
 use tui::{
-    prelude::{
-        Alignment, Backend, Constraint,
-        Direction::{Horizontal, Vertical},
-        Layout,
-    },
-    widgets::{Block, BorderType, Borders},
+    prelude::{Alignment, Backend, Rect},
+    widgets::{Block, BorderType, Borders, Paragraph},
     Frame,
 };
 
+const BOARD_WIDTH: u16 = 36;
+const BOARD_HEIGHT: u16 = 18;
+
 pub fn render<B: Backend>(chess: &mut Chess<B>, frame: &mut Frame<'_, B>) {
-    let center_constraints = [
-        Constraint::Percentage(25),
-        Constraint::Min(50),
-        Constraint::Percentage(25),
-    ];
+    render_outer_block(frame);
+    render_board(chess, frame);
+    render_white_player_nameplate(frame);
+    render_black_player_nameplate(frame);
+}
 
-    let horizontal_layout = Layout::default()
-        .direction(Horizontal)
-        .constraints(center_constraints.as_ref())
-        .split(frame.size());
-
-    let vertical_layout = Layout::default()
-        .direction(Vertical)
-        .constraints(center_constraints.as_ref())
-        .split(horizontal_layout[1]);
-
-    let menu_block = Block::default()
+#[inline(always)]
+fn render_outer_block<B: Backend>(frame: &mut Frame<'_, B>) {
+    let outer_block = Block::default()
         .title("Chess")
         .title_alignment(Alignment::Center)
         .borders(Borders::ALL)
         .border_type(BorderType::Rounded);
-    frame.render_widget(menu_block, vertical_layout[1]);
+    frame.render_widget(outer_block, frame.size());
+}
+
+#[inline(always)]
+fn render_board<B: Backend>(chess: &mut Chess<B>, frame: &mut Frame<'_, B>) {
+    // these centerings do not account for the row numbers and column letters
+    // add 1 to both results to center in relation to the board itself
+    let x_axis = (frame.size().width / 2) - (BOARD_WIDTH / 2);
+    let y_axis = (frame.size().height / 2) - (BOARD_HEIGHT / 2);
+    let board_area = Rect::new(x_axis, y_axis, BOARD_WIDTH, BOARD_HEIGHT);
+    frame.render_widget(chess.game.owned_board(), board_area);
+}
+
+#[inline(always)]
+fn render_white_player_nameplate<B: Backend>(frame: &mut Frame<'_, B>) {
+    let text = "WHITE PLAYER";
+
+    let x_axis = (frame.size().width / 2) - (text.len() / 2) as u16;
+    let y_axis = (frame.size().height / 2) + 14;
+    let text_area = Rect::new(x_axis, y_axis, text.len() as u16, 1);
+    let paragraph = Paragraph::new(text);
+    frame.render_widget(paragraph, text_area);
+}
+
+#[inline(always)]
+fn render_black_player_nameplate<B: Backend>(frame: &mut Frame<'_, B>) {
+    let text = "BLACK PLAYER";
+
+    let x_axis = (frame.size().width / 2) - (text.len() / 2) as u16;
+    let y_axis = (frame.size().height / 2) - 14;
+    let text_area = Rect::new(x_axis, y_axis, text.len() as u16, 1);
+    let paragraph = Paragraph::new(text);
+    frame.render_widget(paragraph, text_area);
 }
